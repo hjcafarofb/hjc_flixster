972a7e08cb68e6b861dddcad620313b9c902c087
Henry Cafaro <hjcafaro@fb.com>
2022-06-15 21:34:10 -0700

    buttons

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const API_key = 'cf209f923568a598d6bbcecff1f78e5d';
 const movie_board = document.getElementsByClassName("movies-grid")[0];
 const search_bar = document.getElementById("search-input");
 const search_form = document.getElementById("search-form");
+const search_button = document.getElementById("search-submit");
 const more_movies_btn = document.getElementById("load-more-movies-btn");
-
+const refresh_btn = document.getElementById("close-search-btn");
+const showing_header_txt = document.getElementById("showing-header-text");
 
 var global_page_id = 1;
 
@@ -133,15 +135,16 @@ function generateMovieCard(movie_data){
 }
 
 function addMovieToPanel(movie_data){
-    //console.log('movie_data: ', movie_data);
+    console.log('movie_data: ', movie_data);
     movie_board.innerHTML += generateMovieCard(movie_data);
 
  }
 
  function processSearch(){
-    document.getElementsByClassName("showing-header")[0].innerHTML = "Results";
+    showing_header_txt.innerHTML = "Results";
     query_string = search_bar.value;
-    console.log(search_bar.value);
+    more_movies_btn.classList.add("closed");
+    refresh_btn.classList.remove("closed");
     search_bar.value = "";
     movie_board.innerHTML = "";
     fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_key}&language=en-US&query=${query_string}`)
@@ -149,9 +152,20 @@ function addMovieToPanel(movie_data){
         .then(data => processList(data));
  }
 
+ function refreshResults(){
+    showing_header_txt.innerHTML = "Now Playing";
+    more_movies_btn.classList.remove("closed");
+    refresh_btn.classList.add("closed");
+    global_page_id = 1;
+    movie_board.innerHTML = "";
+    add_now_playing();
+ }
+
 function addEventListeners(){
     search_form.addEventListener("submit", processSearch);
+    search_button.addEventListener("click", processSearch);
     more_movies_btn.addEventListener("click", add_now_playing);
+    refresh_btn.addEventListener("click", refreshResults);
 }
 
 function processList(movie_data_json){
